33232e23266943d4bc22734417c483e1332b19fd
AlexandrKozich <Qqwert07@mail.ru>
2019-05-23 11:05:00 +0300

    Add Project

---
+++ Project_HomeWork/HomeWork_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite chislo");
+            
+            string perem = Console.ReadLine() ;
+                                          
+            System.Console.WriteLine(CreateRes(perem));
+                       
+        }
+
+        public static string CreateRes(string chislo)
+        {
+           string res1 = "Vvedeno nekorrectnoe chislo";
+            
+           switch (chislo)
+            {
+                case "1":
+                    res1 = ("The entered number corresponds to Monday");
+                    break;
+                case "2":
+                    res1 = ("The entered number corresponds to Tuesday");
+                    break;
+                case "3":
+                    res1 = ("The entered number corresponds to Wednesday");
+                    break;
+                case "4":
+                    res1 = ("The entered number corresponds to Thursday");
+                    break;
+                case "5":
+                    res1 = ("The entered number corresponds to Friday");
+                    break;
+                case "6":
+                    res1 = ("The entered number corresponds to Saturday");
+                    break;
+                case "7":
+                    res1 = ("The entered number corresponds to Sunday");
+                    break;
+
+           }
+
+            return res1;
+        }
+
+    }
+}
+
+
---
+++ Project_HomeWork/HomeWork_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite chislo ");
+
+            int perem = Int32.Parse(Console.ReadLine());
+
+            System.Console.WriteLine(CreateRes(perem));
+
+        }
+
+        public static int CreateRes(int chislo)
+        {
+            int res = chislo;
+          
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{chislo} * {i} = {chislo * i}");
+            }
+           return res;
+        }
+
+           
+    }
+
+    }
